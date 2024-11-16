c230ae2d97dba49bea4273165864553a04fa72c5
Nick <nickmufson1@gmail.com>
2024-11-16 12:35:07 -0500

    update app.ts

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import router from './routes/router';
-import errorHandler from './middleware/errorHandler';
+import router from './routes/router.js';
+import errorHandler from './middleware/errorHandler.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
