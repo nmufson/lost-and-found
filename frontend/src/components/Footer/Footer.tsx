aeccba0076434427dc430382f8f9211ba8ecfeb5
Nick <nickmufson1@gmail.com>
2024-11-29 16:47:19 -0500

    add vercel.json and update footer links

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -5,9 +5,20 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
         <p>
-          Nick Mufson | <a href="mailto:nickmufson1@gmail.com">Email</a> |{' '}
-          <a href="https://www.linkedin.com/in/nicholasmufson/">LinkedIn</a> |{' '}
-          <a href="https://github.com/nmufson">GitHub</a>
+          Nick Mufson |{' '}
+          <a href="mailto:nickmufson1@gmail.com" target="_blank">
+            Email
+          </a>{' '}
+          |{' '}
+          <a href="https://www.linkedin.com/in/nicholasmufson/" target="_blank">
+            {' '}
+            rel="noopener noreferrer" LinkedIn
+          </a>{' '}
+          |{' '}
+          <a href="https://github.com/nmufson" target="_blank">
+            {' '}
+            rel="noopener noreferrer" GitHub
+          </a>
         </p>
       </div>
     </footer>
