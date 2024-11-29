7b7433adaf89b3cc9cde405cbff01d85ed5180fc
Nick <nickmufson1@gmail.com>
2024-11-29 13:59:58 -0500

    update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/router.js';
 import errorHandler from './middleware/errorHandler.js';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 
