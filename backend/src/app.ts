d9fa3d44267d8c08587615c1d1d04e3a87d792a2
Nick <nickmufson1@gmail.com>
2024-12-01 15:56:38 -0500

    add screenshots

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,17 +16,13 @@ app.use(errorHandler);
 
 const PORT = Number(process.env.PORT) || 8080;
 
-console.log('App is starting...');
-console.log('DATABASE_URL:', process.env.DATABASE_URL ? 'Set' : 'Not Set');
-console.log('PORT:', process.env.PORT || 8080);
-
-try {
-  const prisma = new PrismaClient();
-  await prisma.$connect();
-  console.log('Connected to the database successfully!');
-} catch (err) {
-  console.error('Database connection error:', err);
-}
+// try {
+//   const prisma = new PrismaClient();
+//   await prisma.$connect();
+//   console.log('Connected to the database successfully!');
+// } catch (err) {
+//   console.error('Database connection error:', err);
+// }
 
 app.listen(PORT, '0.0.0.0', () =>
   console.log(`Express app listening on port ${PORT}!`),
